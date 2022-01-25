76c9ebb64fa61e5dda5de2bc3ac479b8301c2a83
boring-km <kms0644804@naver.com>
2022-01-26 05:29:18 +0900

    23 Sqflite

--- lib/data/data_source/note_db.dart
+++ lib/data/data_source/note_db.dart
@@ -1,3 +1,47 @@
-class NoteDb {
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:sqflite/sqflite.dart';
 
+class NoteDb {
+  final Database db;
+
+  NoteDb(this.db);
+
+  Future<Note?> getNoteById(int id) async {
+    // SELECT * FROM WHERE id = $id;
+    final maps = await db.query(
+      'note',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (maps.isNotEmpty) {
+      return Note.fromJson(maps.first);
+    }
+    return null;
+  }
+
+  Future<List<Note>> getNotes() async {
+    final maps = await db.query('note');
+    return maps.map((e) => Note.fromJson(e)).toList();
+  }
+
+  Future<void> insertNote(Note note) async {
+    await db.insert('note', note.toJson());
+  }
+
+  Future<void> updateNote(Note note) async {
+    await db.update(
+      'note',
+      note.toJson(),
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
+  }
+
+  Future<void> deleteNote(Note note) async {
+    await db.delete(
+      'note',
+      where: 'id = ?',
+      whereArgs: [note.id],
+    );
+  }
 }
