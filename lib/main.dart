28ccaa934685336519a8aaf158db14d6d620446e
boring-km <kms0644804@naver.com>
2022-01-30 22:52:57 +0900

    31 DI 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note_app/di/provider_setup.dart';
 import 'package:flutter_note_app/presentation/notes/notes_screen.dart';
 import 'package:flutter_note_app/ui/colors.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  // 플랫폼 채널이 위젯 바인딩을 보장
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final providers = await getProviders();
+  runApp(MultiProvider(
+    providers: providers,
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,13 +28,13 @@ class MyApp extends StatelessWidget {
         backgroundColor: darkGray,
         canvasColor: darkGray,
         floatingActionButtonTheme:
-        Theme.of(context).floatingActionButtonTheme.copyWith(
-          backgroundColor: Colors.white,
-          foregroundColor: darkGray,
-        ),
+            Theme.of(context).floatingActionButtonTheme.copyWith(
+                  backgroundColor: Colors.white,
+                  foregroundColor: darkGray,
+                ),
         appBarTheme: Theme.of(context).appBarTheme.copyWith(
-          backgroundColor: darkGray,
-        ),
+              backgroundColor: darkGray,
+            ),
       ),
       home: const NotesScreen(),
     );
